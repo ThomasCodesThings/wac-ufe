b647d7859aa9bdbeff1d5ed83961036eeba680f2
unknown <cernacek,tomas@gmail,com>
2024-02-20 11:02:41 +0100

    ultra senzitivne testiky

--- src/components/cernica-department-app/test/cernica-department-app.spec.tsx
+++ src/components/cernica-department-app/test/cernica-department-app.spec.tsx
@@ -1,11 +1,19 @@
-//import { newSpecPage } from '@stencil/core/testing';
-//import { CernicaDepartmentApp } from '../cernica-department-app';
+import { newSpecPage } from '@stencil/core/testing';
+import { CernicaDepartmentApp } from '../cernica-department-app';
 
-//describe('cernica-department-app', () => {
-  //it('renders', async () => {
-    //const page = await newSpecPage({
-      //components: [CernicaDepartmentApp],
-      //html: `<cernica-department-app></cernica-department-app>`,
-    //});
-  //});
-//});
+describe('cernica-department-app', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [CernicaDepartmentApp],
+      html: `<cernica-department-app></cernica-department-app>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <cernica-department-app>
+        <mock:shadow-root>
+          <cernica-department-list></cernica-department-list>
+        </mock:shadow-root>
+      </cernica-department-app>
+    `);
+  });
+});
--- src/components/cernica-department-edit/test/cernica-department-edit.spec.tsx
+++ src/components/cernica-department-edit/test/cernica-department-edit.spec.tsx
@@ -1,11 +1,76 @@
-//import { newSpecPage } from '@stencil/core/testing';
-//import { CernicaDepartmentEdit } from '../cernica-department-edit';
+import { newSpecPage } from '@stencil/core/testing';
+import { CernicaDepartmentEdit } from '../cernica-department-edit';
 
-//describe('cernica-department-edit', () => {
-  //it('renders', async () => {
-    //const page = await newSpecPage({
-      //components: [CernicaDepartmentEdit],
-      //html: `<cernica-department-edit></cernica-department-edit>`,
-    //});
-  //});
-//});
+describe('cernica-department-edit', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [CernicaDepartmentEdit],
+      html: `<cernica-department-edit></cernica-department-edit>`,
+    });
+
+    expect (page.root).toEqualHtml(`
+      <cernica-department-edit>
+        <mock:shadow-root>
+        <h2 class="title">
+        Editácia záznamu
+      </h2>
+      <md-filled-text-field label="Meno">
+        <md-icon slot="leading-icon">
+         person
+       </md-icon>
+      </md-filled-text-field>
+      <md-filled-text-field label="Priezvisko">
+        <md-icon slot="leading-icon">
+          person
+        </md-icon>
+      </md-filled-text-field>
+      <md-filled-text-field label="Oddelenie">
+        <md-icon slot="leading-icon">
+          home_health
+        </md-icon>
+      </md-filled-text-field>
+     <input id="scheduledDate" name="scheduledDate" type="date">
+<md-outlined-select label="Trvanie (v minútach)">
+        <md-icon slot="leading-icon">
+          schedule
+        </md-icon>
+        <md-select-option value="15">
+          15 minút
+        </md-select-option>
+        <md-select-option value="30">
+          30 minút
+        </md-select-option>
+        <md-select-option value="45">
+          45 minút
+        </md-select-option>
+        <md-select-option value="60">
+          1 hodina
+        </md-select-option>
+        <md-select-option value="120">
+2 hodiny
+        </md-select-option>
+      </md-outlined-select>
+      <md-divider></md-divider>
+      <div class="actions">
+        <md-filled-tonal-button id="delete">
+         <md-icon slot="icon">
+            delete
+          </md-icon>
+          Zmazať
+        </md-filled-tonal-button>
+        <span class="stretch-fill"></span>
+        <md-outlined-button id="cancel">
+          Zrušiť
+        </md-outlined-button>
+        <md-filled-button id="confirm">
+          <md-icon slot="icon">
+            save
+         </md-icon>
+         Uložiť
+       </md-filled-button>
+</div>
+        </mock:shadow-root>
+      </cernica-department-edit>
+    `);
+  });
+});
